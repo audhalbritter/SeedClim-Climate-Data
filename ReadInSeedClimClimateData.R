b299d0edd19847cb25dfcdb7482cfc3ab395eb22
audhalbritter <me@audhalbritter.com>
2016-07-06 21:53:04 +0200

    Clean code

--- ReadInSeedClimClimateData.R
+++ ReadInSeedClimClimateData.R
@@ -1,16 +1,9 @@
 ####################################
 # READ IN SEEDCLIM CLIMATE DATA
 ####################################
-#  Read in ITAS
-setwd("~/Dropbox/seedclim klimadaten/rawdata by Site/Alr/ITAS")
-# Read in UTL
-setwd("~/Dropbox/seedclim klimadaten/rawdata by Site/Alr/UTL")
 
 ### LIBRARIES
 library(plyr)
-
-setwd("~/Dropbox/seedclim klimadaten/rawdata by Site")
-# Import the climate logger data and merge ITAS and UTL data separately for each site
 
 #### IMPORT CLIMATE DATA FOR ALL SITES
 climate <- ldply(as.list(c("Fau", "Alr", "Ulv", "Vik", "Hog", "Lav", "Arh", "Ram", "Gud", "Ovs", "Ves", "Skj")), ImportData)
@@ -37,12 +30,3 @@ temperature %>% filter(year(date) < 2010, !logger %in% c("temp1", "temp2"), site
 temperature %>% filter(year(date) < 2010, !logger %in% c("temp1", "temp2"), site == "Gud") %>% ggplot(aes(x = date, y = value, colour = logger)) + geom_line()
 temperature %>% filter(year(date) < 2010, !logger %in% c("temp1", "temp2"), site == "Ovs") %>% ggplot(aes(x = date, y = value, colour = logger)) + geom_line()
 temperature %>% filter(year(date) < 2010, !logger %in% c("temp1", "temp2"), site == "Ves") %>% ggplot(aes(x = date, y = value, colour = logger)) + geom_line()
-
-# -5cm, PØN and "" are UTL loggers with wrong name. Need to be assigned to the right logger
-pon <- subset(temperature, logger=="PØN")
-t5cm <- subset(temperature, logger=="-5cm")
-nothing <- subset(temperature, logger=="")
-t30 <- subset(temperature, logger=="temp30cm")
-t200 <- subset(temperature, logger=="temp200cm")
-
-
