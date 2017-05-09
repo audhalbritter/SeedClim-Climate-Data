a72182d27d0ac8c281d938cea46835044cfe2588
audhalbritter <me@audhalbritter.com>
2017-05-09 22:08:29 +0200

    Initial commit

---
+++ Figures Science Paper.R
@@ -0,0 +1,92 @@
+#### Figures Sciece Paper ####
+
+temperature %>% 
+  filter(site == "Skj", logger == "temp200cm", value > 25) %>% 
+  left_join(meta.data, by = "site") %>% 
+  ggplot(aes(x = date, y = value, color = T_level)) +
+  geom_line()
+
+
+meta.data <- data.frame(site = c("Fau", "Alr", "Ulv", "Vik", "Hog", "Lav", "Arh", "Ram", "Gud", "Ovs", "Ves",  "Skj"),
+           T_level = rep(c(1,2,3), 4),
+           P_level = c(1,1,1,2,2,2,3,3,3,4,4,4))
+
+
+monthlyTemperature %>% 
+  left_join(meta.data, by = "site") %>% 
+  mutate(T_level = factor(T_level), P_level = factor(P_level)) %>%
+  filter(logger == "temp200cm") %>% 
+  group_by(T_level, date) %>% 
+  summarise(mean = mean(value, na.rm = TRUE)) %>% 
+  ggplot(aes(x = date, y = mean, color = T_level)) +
+  geom_line() +
+  labs(x = "", y = "Monthly air temperature in °C") +
+  scale_color_manual(name = "Temperature level", values = c("orange", "red", "darkred"))
+  
+
+### Gridded data
+load("GriddedDailyClimateData2009-2016.RData", verbose = TRUE)
+
+# calculate monthly values
+monthlyClimate <- climate %>%
+  mutate(Date = dmy(paste0("15-",format(Date, "%b.%Y")))) %>%
+  gather(key = logger, value = value, -Site, -Year, -Month, -Day, -Date) %>% 
+  group_by(Date, logger, Site) %>%
+  summarise(n = n(), value = mean(value), sum = sum(value)) %>%
+  mutate(value = ifelse(logger == "Precipitation", sum, value)) %>% 
+  select(-n, -sum) %>% 
+  rename(date = Date, site = Site) %>% 
+  left_join(meta.data, by = "site") %>%
+  mutate(T_level = factor(T_level), P_level = factor(P_level))
+
+
+monthlyClimate %>% 
+  filter(logger == "Precipitation") %>% 
+  group_by(P_level, date) %>% 
+  summarise(mean = mean(value, na.rm = TRUE)) %>% 
+  ggplot(aes(x = date, y = mean, color = P_level)) +
+  geom_line() +
+  labs(x = "", y = "Monthly precipitation in mm") +
+  scale_color_manual(name = "Precipitation level", values = c("lightblue", "blue", "darkblue", "black"))
+
+
+TetratermT <- monthlyClimate %>% 
+  filter(logger %in% c("Temperature")) %>%
+  filter(month(date) %in% c(6,7,8,9))
+
+
+monthlyClimate %>% 
+  filter(logger %in% c("Precipitation")) %>%
+  filter(year(date) != 2016) %>% 
+  bind_rows(TetratermT) %>% 
+  group_by(logger, T_level, P_level) %>% 
+  summarise(n = n(), mean = mean(value, na.rm = TRUE), sum = sum(value, na.rm = TRUE), se = sd(value, na.rm = TRUE)/sqrt(n), se_sum = sd(value, na.rm = TRUE)*sqrt(n)) %>% 
+  mutate(mean = ifelse(logger == "Precipitation", sum, mean)) %>% 
+  mutate(se = ifelse(logger == "Precipitation", se_sum, se)) %>% 
+  select(-n, -sum, -se_sum) %>% 
+  unite(mean_se, mean, se, sep = "_") %>% 
+  spread(key = logger, value = mean_se) %>% 
+  separate(col = Precipitation, into = c("P_mean", "P_se"), sep = "_", convert = TRUE) %>% 
+  separate(col = Temperature, into = c("T_mean", "T_se"), sep = "_", convert = TRUE) %>%
+  ggplot(aes(x = P_mean, xmin = P_mean - P_se, xmax = P_mean + P_se, 
+             y = T_mean,ymin = T_mean - T_se, ymax = T_mean + T_se,
+             color = P_level, shape = T_level)) +
+  geom_point(size = 3) +
+  geom_errorbar() +
+  geom_errorbarh() +
+  labs(x = "Annual precipitation in mm", y = "Tetraterm temperature in °C") +
+  scale_color_manual(name = "Precipitation level", values = c("lightblue", "blue", "darkblue", "black")) +
+  scale_shape_manual(name = "Temperature level", values = c(17, 16, 15)) +
+  theme(legend.position = "top") +
+  theme_bw()
+
+
+head(monthlyTemperature)
+head(monthlyClimate)
+
+allClimate <- monthlyTemperature %>% 
+  bind_rows(monthlyClimate) %>% 
+  
+
+
+
