178829e376c5e85acefc44ad2416a442a4d13c49
audhalbritter <me@audhalbritter.com>
2017-07-13 11:04:14 +0200

    Also read in txt files

--- iButtonData_FunCaB.R
+++ iButtonData_FunCaB.R
@@ -5,53 +5,82 @@ library("readr")
 
 
 # Extract file names from all iButton files to create dictionary
-myfiles <- dir(path = paste0("/Volumes/FELLES/MATNAT/BIO/Felles/000_Funcab_Seedclim/SeedClimClimateData/iButtondata"), pattern = "csv", recursive = TRUE, full.names = TRUE)
+myfiles <- dir(path = paste0("/Volumes/FELLES/MATNAT/BIO/Felles/007_Funcab_Seedclim/SeedClimClimateData/iButtondata/2016"), pattern = "csv", recursive = TRUE, full.names = TRUE)
+myfiles <- dir(path = paste0("/Volumes/FELLES/MATNAT/BIO/Felles/007_Funcab_Seedclim/SeedClimClimateData/iButtondata/2016"), pattern = "txt", recursive = TRUE, full.names = TRUE)
 myfiles <- tibble(unlist(myfiles))
 
 ddd <- myfiles %>% 
-  separate(col = `unlist(myfiles)`, into = c("a", "b", "c", "d","e","f","g","h","i","siteID","iButtonID"), sep = "/") %>% 
-  select(siteID, iButtonID)
+  separate(col = `unlist(myfiles)`, into = c("a", "b", "c", "d","e","f","g","h","i", "year","siteID", "iButtonID"), sep = "/") %>% 
+  select(year, siteID, iButtonID) %>% print(n = 100)
 
 write.csv(ddd, "iButtonID.csv")
 
-dd <- tibble(dirname("/Volumes/FELLES/MATNAT/BIO/Felles/000_Funcab_Seedclim/SeedClimClimateData/iButtondata/Alrust/3E39E941.csv"))
-colnames(dd) <- "ID"
-dd %>% 
-  separate(col = ID, into = c("a", "b", "c", "d","e","f","g","h","siteID","iButtonID"), sep = "/") %>% 
-  select(siteID, iButtonID)
 
 #### Read in iButtons Function
 ReadIniButtons <- function(textfile){
-  # import body of data
-  dat <- read_csv(textfile, skip = 19)
-  dat$Date <- mdy_hms(dat$`Date/Time`) # convert to date
+  ending <- substr(textfile, nchar(textfile)-4+1, nchar(textfile))
+  print(textfile)
+  # Extract Date, Unit and Value
+ if(ending == ".txt"){
+    dat <- read_delim(textfile, delim = ",", col_names = FALSE)
+    dat$Date <- dmy_hms(dat$X1) # convert to date
+    dat$Unit <- dat$X2
+    dat$Value <- as.numeric(paste(dat$X3, dat$X4, sep = ".")) # merge col 3 and 4 to one number
+    dat <- dat %>% select(-X1, -X2, -X3, -X4)
+  } else if(ending == ".csv"){
+    # import body of data
+    dat <- read_csv(textfile, skip = 19)
+    dat$Date <- mdy_hms(dat$`Date/Time`) # convert to date
+    dat <- dat %>% select(Date, Unit, Value, -`Date/Time`)
+  } else {
+    warning(paste(textfile, "format not recognised")) # warning if logger not recognized
+    dat <- NULL
+  }
   
-  # extract site and file name from file name
+  # Extract siteID, iButtonID and Year from file name
   dat$iButtonID <- basename(textfile)
   textfile2 <- tibble(dirname(textfile))
   colnames(textfile2) <- "ID"
   textfile2 <- textfile2 %>% 
-    separate(col = ID, into = c("a", "b", "c", "d","e","f","g","h","i","siteID"), sep = "/")
+    separate(col = ID, into = c("a", "b", "c", "d","e","f","g","h","i", "Year","siteID"), sep = "/")
   dat$siteID <- textfile2$siteID
-  dat
+  dat$Year <- textfile2$Year
+  return(dat)
 }
+
+
 
 
 # read in iButtonID dictionary
 
 
+
 # MAKE LIST OF ALL TXT FILES AND MERGE THEM TO ONE DATA FRAME
-myfiles <- dir(path = paste0("/Volumes/FELLES/MATNAT/BIO/Felles/000_Funcab_Seedclim/SeedClimClimateData/iButtondata"), pattern = "csv", recursive = TRUE, full.names = TRUE)
+myfiles.1 <- dir(path = paste0("/Volumes/FELLES/MATNAT/BIO/Felles/007_Funcab_Seedclim/SeedClimClimateData/iButtondata"), pattern = "csv", recursive = TRUE, full.names = TRUE)
+myfiles.2 <- dir(path = paste0("/Volumes/FELLES/MATNAT/BIO/Felles/007_Funcab_Seedclim/SeedClimClimateData/iButtondata"), pattern = "txt", recursive = TRUE, full.names = TRUE)
+myfiles.2 <- myfiles.2[-c(1,2,23)] # remove log files
 
-mdat <- plyr::ldply(as.list(myfiles), ReadIniButtons)
+mdat <- plyr::ldply(as.list(myfiles.1), ReadIniButtons)
 head(mdat)
+mdat2 <- plyr::ldply(as.list(myfiles.2), ReadIniButtons)
+head(mdat2)
+mdat <- mdat %>% 
+  bind_rows(mdat2)
 
-mdat %>% 
-  left_join() %>% 
-  mutate(Year = 2016)
+save(mdat, file = "iButton2016.RData")
 
 mdat %>% 
   filter(siteID == "Gudmedalen") %>% 
+  filter(Value > -40, Value < 50) %>%
   ggplot(aes(x = Date, y = Value)) +
   geom_line() +
   facet_wrap(~ iButtonID)
+
+mdat %>% 
+  filter(!iButtonID %in% c("3E369341.csv", "3E3DF841.csv")) %>% # remove 2 iButtons from Gudmeldalen; these loggers need to be checked!!!
+  filter(Value > -40, Value < 50) %>% # crop impossible values
+  group_by(siteID, Date, Year) %>% 
+  summarise(mean = mean(Value)) %>% 
+  ggplot(aes(x = Date, y = mean)) +
+  geom_line() +
+  facet_wrap(~ siteID)
