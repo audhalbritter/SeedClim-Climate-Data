63340e73d5aa45b5c9ffa33a9e3605af39288d8f
Francesca Orinda Holl Jaroszynska <fja062@bio0221005.klientdrift.uib.no>
2017-09-19 09:09:11 +0200

    ibutton data cleaning and plotting

--- iButtonData_FunCaB.R
+++ iButtonData_FunCaB.R
@@ -80,8 +80,19 @@ myfiles <- myfiles[!grepl("unknown", myfiles, ignore.case = TRUE)] # remove log 
 mdat <- map_df(myfiles, ReadIniButtons)
 head(mdat)
 
+#remove .txt/csv suffix from 2017 data so it can join with dictionary
+mdat <- mdat %>% 
+  mutate(iButtonID = if_else(Year == 2017,
+    substr(iButtonID, 1, nchar(iButtonID)-4),
+    iButtonID)) %>% 
+  mutate(iButtonID = if_else(nchar(iButtonID) == 16 ,
+                             gsub(pattern = substr(iButtonID, 3, 8), ""),
+                             iButtonID))
+  
+
 iButtonData <- mdat %>% 
   mutate(Year = as.numeric(Year)) %>% 
+  #filter(!(format(Date, "%Y-%m-%d") < "2016-08-01" & Year == 2017)) %>% 
   full_join(dictionary, by = c("Year", "siteID", "iButtonID"))
 
 save(iButtonData, file = "iButton2016-2017.RData")
@@ -94,34 +105,7 @@ iButtonData %>%
   arrange(siteID) %>% print(n = Inf)
 
 
-iButtonData %>% 
-  filter(siteID == "Lavisdalen") %>% 
-  filter(format(Date, "%Y-%m-%d") == "2015-08-20") %>%
-  filter(Value > -40, Value < 50) %>%
-  ggplot(aes(x = Date, y = Value, color = Block)) +
-  geom_line() +
-  facet_wrap(~ Treatment)
-
-iButtonData %>% 
-  filter(!iButtonID %in% c("3E369341.csv", "3E3DF841.csv")) %>% # remove 2 iButtons from Gudmeldalen; these loggers need to be checked!!!
-  filter(Value > -40, Value < 50) %>% # crop impossible values
-  ggplot(aes(x = Date, y = Value)) +
-  geom_line() +
-  facet_wrap(~ siteID) +
-  ggsave(filename = "ibutton_allSites_allYears.jpg")
-
-
-iButtonData %>% 
-  #filter(siteID == "Hogsete") %>%
-  filter(!iButtonID %in% c("3E369341.csv", "3E3DF841.csv")) %>% # remove 2 iButtons from Gudmedalen; these loggers need to be checked!!!
-  filter(format(Date, "%Y-%m-%d") < "2016-10-20") %>% 
-  filter(Treatment != "NA") %>% 
-  ggplot(aes(x = Date, y = Value, colour = siteID)) +
-  geom_line() +
-  facet_wrap(~ Treatment)
-
-
-# having a look at, and removing, ibuttons that are apparently logging until December 2017
+# Remove ibuttons that are apparently logging until December 2017
 wonkyiButtons <- iButtonData %>% 
   filter(Date > "2017-09-01 00:07:01") %>% 
   distinct(iButtonID) %>% 
@@ -134,9 +118,73 @@ iButtonData %>%
   facet_wrap(~ siteID)
 
 iButtonData <- iButtonData %>% 
-  filter(!iButtonID %in% wonkyiButtons)
+  filter(!iButtonID %in% wonkyiButtons) %>% 
+  #filter(Value > -40, Value < 50) %>% # crop impossible values, but look into a smoother option
+  filter(!iButtonID %in% c("3E369341.csv", "3E3DF841.csv")) %>% # remove 2 iButtons from Gudmeldalen; these loggers need to be checked!!!
+  filter(!(Date < ReplacementDate & Year == 2016)) %>%  # remove values in 2015 before ibuttons have been placed
+  #filter(!(Date < ReplacementDate & Year == 2017)) %>% 
+  filter(!(Date > RemovalDate & Year == 2016)) %>% 
+  filter(!(Year == 2017))
+  #filter(!(Date > RemovalDate & Year == 2017))
+  #filter(!is.na(Treatment))
 
-# we need to find some way in the code to allow ibutton id to change from removal date to replacement date...
+ibuttonID2017 <- iButtonData %>% 
+  filter(Year == 2017) %>% 
+  distinct(iButtonID)
+
+ibutton2017 <- iButtonData %>% 
+  filter(Year == 2017)
+
+# need to check ibuttons that don't seem to be logging regularly. happens in several sites.
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#1C9099", "#A6BDDB", "#ECE2F0", "orange3")
+
+
+iButtonData %>% 
+  filter(siteID == "Lavisdalen") %>% 
+  filter(format(Date, "%Y-%m-%d") == "2015-08-20") %>%
+  ggplot(aes(x = Date, y = Value, color = Block)) +
+  geom_line() +
+  facet_wrap(~ Treatment)
+
+iButtonData %>% 
+  ggplot(aes(x = Date, y = Value, colour = Treatment)) +
+  geom_line() +
+  facet_wrap(~ siteID) +
+  ggsave(filename = "ibutton_allSites_2016.jpg", path = "/Users/fja062/Documents")
+
+
+minmax <- iButtonData %>% 
+  #mutate(bryophytes = if_else(Treatment %in% c("B", "FB", "GB", "FGB"), "removed", "remained")) %>% 
+  group_by(siteID, Treatment, week = week(Date)) %>% 
+  mutate(min = min(Value, na.rm = TRUE),
+            max = max(Value, na.rm = TRUE)) %>% 
+  arrange(siteID) %>% ungroup() %>% 
+  gather(range, temperature, min, max)
+
+minmax %>% 
+  filter(siteID == "Fauske") %>% 
+  ggplot(aes(x = week, y = temperature, colour = range)) +
+  geom_line() +
+  facet_wrap(~ Treatment) +
+  scale_color_manual(values = cbPalette)
+  ggsave(filename = "ibutton_minTemp_2016.jpg", path = "/Users/fja062/Documents")
+
+
+ibutton2017 %>% 
+  filter(siteID == "Lavisdalen") %>%
+  filter(Date < "2016-07-15" & Year == 2017) %>%
+  filter(Date > "2016-06-20") %>% 
+  ggplot(aes(x = Date, y = Value)) +
+  geom_line()
+
+
+
+
+
+
+
+# and we need to add figures to the .gitignore
+
 
 #####################################
 ######## PHENOLOGY PLOTTING #########
