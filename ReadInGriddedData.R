9bae4b4aec032ab88443412e962b4062bb4620a1
audhalbritter <me@audhalbritter.com>
2021-04-30 11:26:05 +0200

    update read in file and make compatable with seedclim

--- ReadInGriddedData.R
+++ ReadInGriddedData.R
@@ -1,4 +1,4 @@
-### READ IN GRIDDED CLIMATE DATA 2009 - 2017
+### READ IN GRIDDED CLIMATE DATA 2009 - XXX
 
 # Explanation for variables
 # TAM er temperatur (døgnmiddel)
@@ -7,44 +7,54 @@
 # NNM - midlere skydekke (i 8-deler)
 # RR - døgnnedbør
 
-
 # LIBRARIES
+library("dataDownloader")
 library("lubridate")
 library("tidyverse")
 library("ggplot2")
+
+
+# download data from OSF
+get_file(node = "npfa9",
+         file = "Gridded_Climate_Data_2009-2019.zip",
+         path = "climate/data/",
+         remote_path = "8_Environmental_data/Raw_data")
+
+zipFile <- "climate/data/Gridded_Climate_Data_2009-2019.zip"
+outDir <- "climate/data/"
+unzip(zipFile, exdir = outDir)
 
 # FUNCTIONS
 # Function to read in the data
 ReadInFiles <- function(textfile){
   dd <- read.table(textfile, colClasses = "character")
-  colnames(dd) <- c("Site", "Year", "Month", "Day", "Temperature", "RelAirMoisture", "Wind", "CloudCover", "Precipitation") # rename variables
+  colnames(dd) <- c("siteID", "year", "month", "day", "temperature", "rel_air_moisture", "wind", "cloud_cover", "precipitation") # rename variables
   dd <- dd[-1,] # remove first columne
-  dd$Date <- ymd(paste(dd$Year, dd$Month, dd$Day)) # create date object
+  dd$date <- ymd(paste(dd$year, dd$month, dd$day)) # create date object
   return(dd)
 }
 
 # Connect to data on P drive
-myfiles <- list.files(path="/Volumes/FELLES/MATNAT/BIO/Ecological and Environmental Change/SeedClimClimateData/met-data/GriddedClimateData2009-2017", pattern='\\.dat$', recursive = TRUE, full.names = TRUE)
+myfiles <- list.files(path ="climate/data/AH2019/", pattern='\\.dat$', recursive = TRUE, full.names = TRUE)
 
 # make a list of textfiles
-gridclimate <- plyr::ldply(myfiles, ReadInFiles)
-head(gridclimate)
+climate_gridded_raw <- plyr::ldply(myfiles, ReadInFiles)
 
-climate_all <- gridclimate %>% 
+climate_gridded <- climate_gridded_raw %>% 
+  as.tibble() %>% 
   # replace site names by real names
-  mutate(Site = plyr::mapvalues(Site, c("888001", "888002", "888003", "888004", "888005", "888006", "888007", "888008", "888009", "888010", "888011", "888012", "888191", "888192"), c("Alr", "Arh", "Fau", "Gud", "Hog", "Lav", "Ovs", "Ram", "Skj", "Ulv", "Ves", "Vik", "Joa", "Lia"))) %>% 
-  mutate(Site = factor(Site, levels = c("Ulv", "Lav", "Gud", "Skj", "Alr", "Hog", "Ram", "Ves", "Fau", "Vik", "Arh", "Ovs", "Joa", "Lia"))) %>% 
-  mutate(Year = as.numeric(Year)) %>% 
-  mutate(Temperature = as.numeric(Temperature), RelAirMoisture = as.numeric(RelAirMoisture), Wind = as.numeric(Wind), CloudCover = as.numeric(CloudCover), Precipitation = as.numeric(Precipitation)) %>% 
-  # remove duplicates
-  distinct(Site, Year, Month, Day, Date, Temperature, RelAirMoisture, Wind, CloudCover, Precipitation) %>% 
-  as_tibble()
+  mutate(siteID = plyr::mapvalues(siteID, c("888001", "888002", "888003", "888004", "888005", "888006", "888007", "888008", "888009", "888010", "888011", "888012", "888191", "888192"), 
+                                  c("Alrust", "Arhelleren", "Fauske", "Gudmedalen", "Hogsete", "Lavisdalen", "Ovstedalen", "Rambera", "Skjelingahaugen", "Ulvehaugen", "Veskre", "Vikesland", "Joasete", "Liahovden"))) %>% 
+  mutate(siteID = factor(siteID, levels = c("Ulvehaugen", "Lavisdalen", "Gudmedalen", "Skjelingahaugen", "Alrust", "Hogsete", "Rambera", "Veskre", "Fauske", "Vikesland", "Arhelleren", "Ovstedalen", "Joasete", "Liahovden"))) %>% 
+  mutate(across(c(temperature:precipitation), as.numeric)) %>% 
+  select(date, siteID, temperature:precipitation) %>% 
+  pivot_longer(cols = temperature:precipitation, names_to = "variable", values_to = "value")
 
-climate <- climate_all %>% 
-  filter(!Site %in% c("Joa", "Lia"))
+climate <- climate_gridded %>% 
+  filter(!siteID %in% c("Joasete", "Liahovden"))
 
-climate_threeD <- climate_all %>% 
-  filter(Site %in% c("Vik", "Joa", "Lia"))
+climate_threeD <- climate_gridded %>% 
+  filter(siteID %in% c("Vikesland", "Joasete", "Liahovden"))
 
 # Change directory
 write_csv(climate, path = "GriddedDailyClimateData2009-2019.csv", col_names = TRUE)
